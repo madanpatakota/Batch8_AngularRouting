790a8921687d03c1a8322014dfe1059748bbbddd
Madan <madan.patakota@gmail.com>
2023-08-17 10:32:16 +0530

    Routers basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { SchoolComponent } from './school/school.component';
+import { SchoolDetailsComponent } from './school-details/school-details.component';
+import { Routes , RouterModule } from '@angular/router';
+
+// http://localhost:60934/main      ---- Maincontent
+// http://localhost:60934/school     ---- schoolcontent
+// http://localhost:60934/schooldetails  ---- schooldetailscontent
+// path : 'main'    maincomponent
+// path : 'school'  schoolcomponent
+// path : 'schooldetails' schooldetailcomponent
+
+const AppRoutes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'school', component: SchoolComponent },
+  { path: 'school-details', component: SchoolDetailsComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SchoolComponent,
+    SchoolDetailsComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule , RouterModule.forRoot(AppRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/school-details/school-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-school-details',
+  templateUrl: './school-details.component.html',
+  styleUrls: ['./school-details.component.css']
+})
+export class SchoolDetailsComponent {
+
+}
